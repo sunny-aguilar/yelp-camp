e417455e5a678f09ccd7ffbc612055e8172464c4
sunny-aguilar <sunny.aguilar@gmail.com>
2020-06-29 16:18:00 -0700

    yelp camp

--- app.js
+++ app.js
@@ -21,9 +21,10 @@ app.get('/campgrounds', function(req, res) {
         {name: 'American River North Fork', image: 'https://images.pexels.com/photos/2398220/pexels-photo-2398220.jpeg'},
         {name: 'Saramento River', image: 'https://images.pexels.com/photos/756780/pexels-photo-756780.jpeg'},
         {name: 'Cache Creek', image: 'https://images.pexels.com/photos/45241/tent-camp-night-star-45241.jpeg'},
-        {name: 'South Lake Tahoe', image: 'https://images.pexels.com/photos/2516423/pexels-photo-2516423.jpeg'},
+        {name: 'South Lake Tahoe', image: 'https://images.pexels.com/photos/2516423/pexels-photo-2516423.jpeg'}
     ];
     res.send('Campgrounds page');
+    res.render('campgrounds');
 });
 
 
