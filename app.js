89701db85f7799fd22e4c8a5de1891b1aea9ac6b
sunny-aguilar <sunny.aguilar@gmail.com>
2020-06-30 18:32:34 -0700

    yelp camp

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ const campgrounds = [
     {name: 'Lake Berryesa', image: 'https://images.pexels.com/photos/699558/pexels-photo-699558.jpeg'},
     {name: 'American River North Fork', image: 'https://images.pexels.com/photos/2398220/pexels-photo-2398220.jpeg'},
     {name: 'Saramento River', image: 'https://images.pexels.com/photos/756780/pexels-photo-756780.jpeg'},
+    {name: 'Folsom Lake', image: 'https://images.pexels.com/photos/756780/pexels-photo-756780.jpeg'},
     {name: 'Cache Creek', image: 'https://images.pexels.com/photos/45241/tent-camp-night-star-45241.jpeg'},
     {name: 'South Lake Tahoe', image: 'https://images.pexels.com/photos/2516423/pexels-photo-2516423.jpeg'}
 ];
