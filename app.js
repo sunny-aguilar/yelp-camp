84a3d94bcbdb157dc85c35731d29eca42a38fd42
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-02 08:56:42 -0700

    yelp camp

--- app.js
+++ app.js
@@ -63,6 +63,6 @@ app.get('/campgrounds/new', function(req, res) {
 
 
 
-// start server
+// start server & listen for connections
 const port = 3000;
 app.listen(port, console.log(`Server is listening on port ${port}`));
