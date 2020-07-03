15a805cfbfa15ddfd6055a7d8397d63b83d1736e
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-02 18:08:50 -0700

    yelp camp

--- databases/cats.js
+++ databases/cats.js
@@ -4,7 +4,7 @@
 const mongoose = require('mongoose');
 
 // connect to the mongoDB
-mongoose.connect('mongodb://localhost/cat_app', {
+mongoose.connect('mongodb://alocalhost/cat_app', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
