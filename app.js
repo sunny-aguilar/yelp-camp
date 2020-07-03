23b25185d637aaa7f80a1845c404f3dae39b0888
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-03 00:12:42 -0700

    yelp camp

--- app.js
+++ app.js
@@ -46,17 +46,6 @@ const Campground = mongoose.model('Campground', campgroundSchema);
 
 
 
-// app data --> will be placed in database later
-const campgrounds = [
-    {name: 'Putah Creek', image: 'https://images.pexels.com/photos/1687845/pexels-photo-1687845.jpeg'},
-    {name: 'Lake Solano', image: 'https://images.pexels.com/photos/354611/pexels-photo-354611.jpeg'},
-    {name: 'Lake Berryesa', image: 'https://images.pexels.com/photos/699558/pexels-photo-699558.jpeg'},
-    {name: 'American River North Fork', image: 'https://images.pexels.com/photos/2398220/pexels-photo-2398220.jpeg'},
-    {name: 'Sacramento River', image: 'https://images.pexels.com/photos/349732/pexels-photo-349732.jpeg'},
-    {name: 'Folsom Lake', image: 'https://images.pexels.com/photos/619950/pexels-photo-619950.jpeg'},
-    {name: 'Cache Creek', image: 'https://images.pexels.com/photos/45241/tent-camp-night-star-45241.jpeg'},
-    {name: 'South Lake Tahoe', image: 'https://images.pexels.com/photos/2516423/pexels-photo-2516423.jpeg'}
-];
 
 
 
