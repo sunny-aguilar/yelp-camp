73154bf8b02252500ceeeb539732d4d92ec10288
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-07 20:11:56 -0700

    yelp camp

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -74,7 +74,14 @@ router.get('/:id', function(req, res) {
 
 // edit campgrounds
 router.get('/:id/edit', function(req, res) {
-    res.render('campgrounds/edit', {campground: campground});
+    Campground.findById(req.params.id, function(err, foundCampground) {
+        if (err) {
+            res.redirect('/campgrounds');
+        }
+        else {
+            res.render('campgrounds/edit', {campground: campground});
+        }
+    });
 });
 
 // update campground route
