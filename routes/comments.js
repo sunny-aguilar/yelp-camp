2f24b70e4635e963b68990a7dcadec86486882e6
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-07 19:27:59 -0700

    yelp camp

--- routes/comments.js
+++ routes/comments.js
@@ -42,7 +42,6 @@ router.post('/', isLoggedIn, function(req, res) {
                     comment.save();
                     campground.comments.push(comment);
                     campground.save();
-                    console.log(comment); // delete this comment
                     res.redirect('/campgrounds/' + campground._id);
                 }
             });
