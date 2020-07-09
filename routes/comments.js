27810584316227d634004f454b699d3eb467d507
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-08 22:38:25 -0700

    yelp camp

--- routes/comments.js
+++ routes/comments.js
@@ -63,7 +63,7 @@ router.get('/:comment_id/edit', checkCommentOwnership, function(req, res) {
 });
 
 // handle edit comments logic
-router.put('/:comment_id', function(req, res) {
+router.put('/:comment_id', checkCommentOwnership, function(req, res) {
     Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComments) {
         if (err) {
             res.redirect('back');
