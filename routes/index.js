10d95e939501d36777d01ca98ec0cd7bade4b320
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-09 16:35:07 -0700

    yelp camp

--- routes/index.js
+++ routes/index.js
@@ -55,12 +55,5 @@ router.get('/logout', function(req, res) {
 });
 
 
-// // middleware - checks if user is logged in
-// function isLoggedIn(req, res, next) {
-//     if (req.isAuthenticated()) {
-//         return next();
-//     }
-//     res.redirect('/login');
-// }
 
 module.exports = router;
