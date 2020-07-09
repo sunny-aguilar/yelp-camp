2a81b06424af139c1261844257cba4307fef716b
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-08 22:50:21 -0700

    yelp camp

---
+++ middleware/index.js
@@ -0,0 +1,16 @@
+// all the middleware here
+
+const middlewareObj = {};
+
+middlewareObj.checkCampgroundOwnership = function() {
+    //
+}
+
+middlewareObj.checkCommentOwnership = function() {
+    //
+
+}
+
+
+
+module.exports = middlewareObj;
