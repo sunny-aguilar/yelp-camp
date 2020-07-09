df37b51995b18efc9f3a8a3e08ca7da75b5a88b2
sunny-aguilar <sunny.aguilar@gmail.com>
2020-07-09 16:07:31 -0700

    yelp camp

--- routes/comments.js
+++ routes/comments.js
@@ -88,7 +88,7 @@ router.delete('/:comment_id', middleware.checkCommentOwnership, function(req, re
         }
         else {
             // flash message
-            req.flash('success', '');
+            req.flash('success', 'Comment deleted.');
             res.redirect('/campgrounds/' + req.params.id);
         }
     });
